32e935960859bf39bd4fe008a9d214200e5a7bf8
Dennis Kibet <dennisrkibet@gmail.com>
2024-10-01 11:58:19 +0300

    passed the earthquake test

--- server/app.py
+++ server/app.py
@@ -1,27 +1,45 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 
 from models import db, Earthquake
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    body = {'message': 'Flask SQLAlchemy Lab 1'}
+    body = {"message": "Flask SQLAlchemy Lab 1"}
     return make_response(body, 200)
 
+
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquakes(id):
+    earthquake = Earthquake.query.get(id)
+    if earthquake:
+        return (
+            jsonify(
+                {
+                    "id": earthquake.id,
+                    "location": earthquake.location,
+                    "magnitude": earthquake.magnitude,
+                    "year": earthquake.year,
+                }
+            ),
+            200,
+        )
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
